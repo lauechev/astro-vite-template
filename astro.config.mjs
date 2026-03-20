96039ca4d19b9ae0fdb36d34e50fac8313235f4c
lauechev <laura.echeverri3@gmail.com>
2026-03-20 17:36:55 -0500

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   // Update these per project for GitHub Pages deployment
-  site: 'https://your-username.github.io',
-  base: '/repo-name', // Change to match your repository name
+  site: 'https://lauechev.github.io',
+  base: '/astro-vite-template',
   output: 'static',
   integrations: [sitemap()],
 })
